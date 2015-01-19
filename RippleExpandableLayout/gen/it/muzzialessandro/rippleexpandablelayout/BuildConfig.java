81f7526368580601e0cbea801b5cfb4835e1a00d
AleBestia <alessandromuzzi17@gmail.com>
2015-01-19 16:06:12 +0100

    Erase

--- RippleExpandableLayout/gen/it/muzzialessandro/rippleexpandablelayout/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package it.muzzialessandro.rippleexpandablelayout;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
